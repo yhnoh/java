64c67fae2f99aa374303bb38df45926fabb365a0
yhnoh <tedgom19@gmail.com>
2024-12-05 23:08:32 +0900

    webclient에 대해서 알아보기

--- thread/untitled/src/main/java/org/example/bounded/BoundedQueueV3.java
+++ thread/untitled/src/main/java/org/example/bounded/BoundedQueueV3.java
@@ -18,26 +18,38 @@ public class BoundedQueueV3 implements BoundedQueue {
     }
 
     @Override
-    public void put(String data) {
-        synchronized (producerLock){
-            while(queue.size() == max) {
-                log("[put] 큐가 가득 참, 생산자 대기");
-                sleep(1000);
+    public synchronized void put(String data) {
+
+        while(queue.size() == max) {
+            log("[put] 큐가 가득 참, 생산자 대기");
+            try {
+                wait(); // 락 반납하고, 대기 상태 RUNNABLE -> WAITING, 락 반납
+                log("[put] 생산자 깨어남");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-            queue.offer(data);
         }
+        queue.offer(data);
+        log("[put] 생산자 데이타 저장, notify() 호출");
+        notify(); // WAITING -> RUNNABLE
     }
 
     @Override
-    public String take() {
-        synchronized (consumerLock){
-            while (queue.isEmpty()) {
-                log("[put] 큐에 데이터 없음, 생산자 대기");
-                sleep(1000);
+    public synchronized String take() {
+        while (queue.isEmpty()) {
+            log("[take] 큐에 데이터 없음, 생산자 대기");
+            try {
+                wait();
+                log("[take] 소비자 깨어님");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-
-            return queue.poll();
         }
+
+        String data = queue.poll();
+        log("[take] 소비자 데이터 획득, notify() 호출");
+        notify(); // 대기 스레드, WAIT -> BLOCKED
+        return data;
     }
 
     @Override
