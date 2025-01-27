5d78576a9e66315cb9ac4311430b6e948dc64a5f
yhnoh <tedgom19@gmail.com>
2025-01-27 15:52:10 +0900

    자바 Stream에 대해서 알아보기

--- interface/compare/src/main/java/org/example/io/encoding/EncodingMain1.java
+++ interface/compare/src/main/java/org/example/io/encoding/EncodingMain1.java
@@ -1,6 +1,5 @@
 package org.example.io.encoding;
 
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
@@ -8,11 +7,14 @@ import java.util.Arrays;
  * 컴퓨터는 0과1만을 인지할 수 있기 때문에, 사람이 이해하고 있는 문자를 컴퓨터가 이해할 수 없다.
  * 때문에 사람의 언어를 컴퓨터가 인지하기 위하여 여러 규칙을 만들고 해당 규칙에 따라서 바이트로 변환해주는 과정을 통해서 컴퓨터가 문자를 인지할 수 있도록하였다.
  * 위와같은 과정을 인코딩 디코딩 과정이라 한다.
+ * 문자집합(Character Set)은 문자를 표현하기 위한 여러 규직이다.
  *
  * 인코딩: 문자(언어)를 컴퓨터가 이해할 수 있는 바이트로 변환
  * 디코딩: 바이트 문자를 문자(언어)로 변환
+ *
+ * https://codingpractices.tistory.com/entry/%EC%9D%B8%EC%BD%94%EB%94%A9-vs-%EB%94%94%EC%BD%94%EB%94%A9-%EC%A0%95%ED%99%95%ED%95%98%EA%B2%8C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0
  */
-public class EncodingTest {
+public class EncodingMain1 {
 
 
     public static void main(String[] args) {
