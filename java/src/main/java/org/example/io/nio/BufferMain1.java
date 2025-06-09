05a6e9042d80dbbcf0805d48cfdd137e0708a74b
yhnoh <tedgom19@gmail.com>
2025-06-09 21:39:45 +0900

    nio에 대해서 알아보기

--- java/src/main/java/org/example/io/nio/BufferMain1.java
+++ java/src/main/java/org/example/io/nio/BufferMain1.java
@@ -1,7 +1,44 @@
 package org.example.io.nio;
 
+import java.nio.ByteBuffer;
+
 public class BufferMain1 {
 
     public static void main(String[] args) {
+
+
+        byte[] bytes = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+        // ByteBuffer 초기화
+        ByteBuffer byteBuffer = ByteBuffer.allocate(bytes.length);
+        System.out.println("ByteBuffer 초기화");
+        System.out.println("capacity = " + byteBuffer.capacity() + ", limit = " + byteBuffer.limit() + ", position = " + byteBuffer.position() + ", remaining = " + byteBuffer.remaining());
+        System.out.println();
+
+        // ByteBuffer 데이터 쓰기, 원본의 offset 부터 지정한 길이 만큼 ByteBuffer에 데이터를 쓴다.
+        byteBuffer.put(bytes, 0, bytes.length);
+        System.out.println("ByteBuffer 데이터 쓰기");
+        System.out.println("capacity = " + byteBuffer.capacity() + ", limit = " + byteBuffer.limit() + ", position = " + byteBuffer.position() + ", remaining = " + byteBuffer.remaining());
+        System.out.println();
+
+        // ByteBuffer 데이를 읽거나 쓰기위하여 준비, limit를 position으로 설정하고, position을 0으로 설정한다.
+        byteBuffer.flip();
+        System.out.println("ByteBuffer 데이를 읽거나 쓰기위하여 준비");
+        System.out.println("capacity = " + byteBuffer.capacity() + ", limit = " + byteBuffer.limit() + ", position = " + byteBuffer.position() + ", remaining = " + byteBuffer.remaining());
+        System.out.println();
+
+        // ByteBuffer 데이터 읽기
+        System.out.println("ByteBuffer 데이터 읽기");
+        while (byteBuffer.hasRemaining()) {
+            char c = (char) byteBuffer.get();
+            System.out.println("char = " + c + ", capacity = " + byteBuffer.capacity() + ", limit = " + byteBuffer.limit() + ", position = " + byteBuffer.position() + ", remaining = " + byteBuffer.remaining());
+        }
+        System.out.println();
+
+        // ByteBuffer Clear, 실제 데이터가 지워지지지는 않는다.
+        byteBuffer.clear();
+        System.out.println("ByteBuffer Clear");
+        System.out.println("capacity = " + byteBuffer.capacity() + ", limit = " + byteBuffer.limit() + ", position = " + byteBuffer.position() + ", remaining = " + byteBuffer.remaining());
+        System.out.println();
+
     }
 }
--- java/src/main/java/org/example/io/nio/README.md
+++ java/src/main/java/org/example/io/nio/README.md
@@ -1,17 +1,33 @@
 ### Buffer
+- `java.nio.Buffer`는 `java.nio.Channel`에서 ***데이터를 읽거나 쓰기 위해 사용하는 컨테이너 역할***을 한다.
+    - `java.io`에서 제공하는 스트림의 경우 1바이트 단위로 데이터를 읽거나 쓰지만 `java.nio`에서는 기본적으로 버퍼 단위로 데이터를 읽거나 쓴다.
 
-- `java.nio.Buffer`는 `java.nio.Channel`에서 데이터를 읽거나 쓰기 위해 사용하는 클래스다.
-  - `java.io`에서 제공하는 스트림의 경우 1바이트 단위로 데이터를 읽거나 쓰지만 `java.nio`에서는 기본적으로 버퍼 단위로 데이터를 읽거나 쓴다.
-- 
+#### Buffer의 특징
+- 원시 데이터 타입 버퍼 제공
+    - Buffer의 하위 클래스로 `ByteBuffer, CharBuffer, DoubleBuffer, FloatBuffer, IntBuffer, LongBuffer, ShortBuffer`가 구현되어 있는것을 확인할 수 있듯이 다양한 원시 데이터 타입을 제공한다.
+- Non DirectBuffer & Direct Buffer
+  - 
+- 인덱스 기반으로 하는 데이터 처리
+  - Buffer에 데이터 저장전, 저장후, 읽기 및 쓰기, 자원정리등의 행위들을 인덱스기반으로 처리하게 된다.
+
+#### Buffer의 주요 구성 요소
+- 인덱스 기반으로 데이터를 처리하기 위하여 Buffer에서 제공하는 몇가지 구성 요소들이 존재한다.
+  - `capacity`: 버퍼가 데이터를 담을 수 있는 총량
+  - `limit`: 버퍼에서 데이터를 읽거나 쓰기 위한 마지막 위치
+  - `position`: 버퍼에서 데이터를 읽거나 쓰기 위한 현재의 위치
+  - `mark`: position의 위치를 기억하기 위한 변수
+    - `mark()` 호출시 현재의 `position`을 기억하고, `reset()` 호출시 마킹한 위치로 `position`을 되돌린다.
+
+#### Buffer의 일반적인 사용 흐름
+
 
 ### Channel
-- `java.nio.Channel`은 `java.io`에서 제공하는 스트림처럼 I/O 작업을 수행하는 클래스다.
-  - 스트림의 경우 단방향인 반면 채널은 읽기 및 쓰기 작업이 하나의 객체로 가능하다.
-- 
 
+- `java.nio.Channel`은 `java.io`에서 제공하는 스트림처럼 I/O 작업을 수행하는 클래스다.
+    - 스트림의 경우 단방향인 반면 채널은 읽기 및 쓰기 작업이 하나의 객체로 가능하다.
+-
 
 ### Selector
-
 
 ## Reference
 
