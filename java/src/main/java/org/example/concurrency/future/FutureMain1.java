d21c696cece305256e89af0481c9eb1f52ee76f1
yhnoh <tedgom19@gmail.com>
2025-07-26 17:18:17 +0900

    Future에 대해서 알아보기

--- java/src/main/java/org/example/concurrency/future/FutureMain1.java
+++ java/src/main/java/org/example/concurrency/future/FutureMain1.java
@@ -3,12 +3,17 @@ package org.example.concurrency.future;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.RunnableFuture;
 
 
+/**
+ * <p>Future 예제</p>
+ * Runnable과 Callable을 사용하여 Future를 생성하는 예제 <br/>
+ * Future를 통해서 작업의 결과물을 가져올 수 있는 방법 확인
+ */
 public class FutureMain1 {
 
     private static final Logger log = LoggerFactory.getLogger(FutureMain1.class);
@@ -21,26 +26,34 @@ public class FutureMain1 {
 
 
     public static void createFutureByRunnable() {
-        try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
-            Future<?> futrue = executor.submit(() -> {
-                log.info("createFutureByRunnable() Hello World");
-            });
 
-            futrue.get();
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
+        Runnable runnable = () -> {
+            log.info("Runnable Task is running");
+        };
+        RunnableFuture<String> future = new FutureTask<>(runnable, "Runnable Task Completed");
+        Thread thread = new Thread(future);
+
+        try {
+            thread.start();
+            log.info("future result = {}", future.get());
+        } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static void createFutureByCallable() {
-        try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
-            Future<String> submit = executor.submit(() -> "Hello World");
-            log.info("createFutureByCallable() " + submit.get());
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
+        Callable<String> callable = () -> {
+            log.info("Callable Task is running");
+            return "Callable Task Completed";
+        };
+
+        RunnableFuture<String> future = new FutureTask<>(callable);
+        Thread thread = new Thread(future);
+
+        try {
+            thread.start();
+            log.info("future result = {}", future.get());
+        } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
     }
